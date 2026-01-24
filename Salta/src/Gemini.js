cf09f9cdec56a74e6d354f4807966005e9fa8cfa
namanydv <namanofficial91@gmail.com>
2026-01-24 17:07:24 +0530

    feat: update Gemini integration and environment configuration

--- Salta/src/Gemini.js
+++ Salta/src/Gemini.js
@@ -3,17 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const ai = new GoogleGenAI({
+const GoogleAi = new GoogleGenAI({
   apiKey: process.env.API_KEY,
 });
 
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents: "Explain how AI works in simple words",
-  });
-
-  // NEW SDK supports this
+async function main (){
+  const response = await GoogleAi.models.generateContent({
+    model:"gemini-2.5-flash",
+    contents:"Aur Kasin ho bhai",
+  })
   console.log(response.text);
 }
 
