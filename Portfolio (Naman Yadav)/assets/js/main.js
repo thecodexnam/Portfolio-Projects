5d2b87e204a6b5b570e4b7a039a565c734f55faf
namanydv <namanofficial91@gmail.com>
2026-01-27 10:10:50 +0530

    feat: add responsive portfolio website with HTML, CSS, and JavaScript
    - Created index.html for the main structure of the portfolio.
    - Added styles.css for custom styling and responsive design.
    - Implemented JavaScript for interactive features and animations.
    - Included various assets such as images and PDFs for projects and resume.
    - Developed README.md to provide an overview and usage instructions.
    - Integrated Swiper for project display and smooth scrolling effects.

---
+++ Portfolio (Naman Yadav)/assets/js/main.js
@@ -0,0 +1,101 @@
+/*=============== HOME SPLIT TEXT ===============*/
+const {animate, text, stagger} = anime;
+
+const { chars: chars1 } = text.split('.home__profession-1', {chars:true});
+const { chars: chars2 } = text.split('.home__profession-2', {chars:true});
+
+animate(chars1,{
+    y:[
+        {to:['100%','0%']},
+        {to: '-100%', delay: 4000, ease: 'in(3'}
+    ],
+    duration:900,
+    ease:'out(3)',
+    delay: stagger(80),
+    loop:true,
+})
+
+animate(chars2,{
+    y:[
+        {to:['100%','0%']},
+        {to: '-100%', delay: 4000, ease: 'in(3'}
+    ],
+    duration:900,
+    ease:'out(3)',
+    delay: stagger(80),
+    loop:true,
+})
+
+/*=============== SWIPER PROJECTS ===============*/
+const swiperProjects = new Swiper('.projects__swiper', {
+  loop: true,
+  spaceBetween:24,
+  slidesPerView: 'auto',
+  grabCursor: true,
+  speed:600,
+  
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  autoplay:{
+    delay:3000,
+    disableOnInteraction: false
+  }
+});
+
+
+/*=============== WORK TABS ===============*/
+    const tabs = document.querySelectorAll('[data-target]');
+    const tabContents = document.querySelectorAll('[id="experience"], [id="education"]');
+
+    tabs.forEach((tab) => {
+        tab.addEventListener('click', () => {
+            const targetSelector = tab.dataset.target;
+            const targetContent = document.querySelector(targetSelector);
+
+            // Disable all content and active tabs
+            tabContents.forEach((content) => {
+                content.classList.remove('work-active');
+            });
+            tabs.forEach((t) => {
+                t.classList.remove('work-active');
+            });
+
+            // Active the tab and corresponding content
+            tab.classList.add('work-active');
+            if (targetContent) {
+                targetContent.classList.add('work-active');
+            }
+        });
+    });
+
+
+
+
+/*=============== SERVICES ACCORDION ===============*/
+const servicesButton = document.querySelectorAll('.services_-button')
+
+
+
+/*=============== TESTIMONIALS OF DUPLICATE CARDS ===============*/
+
+
+/*=============== COPY EMAIL IN CONTACT ===============*/
+
+
+/*=============== CURRENT YEAR OF THE FOOTER ===============*/ 
+
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+/*=============== CUSTOM CURSOR ===============*/
+
+
+/* Hide custom cursor on links */
+
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
