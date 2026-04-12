4abef5997e2b2fcf4be0cf7e6697850959a90a87
namanydv <namanofficial91@gmail.com>
2026-04-12 20:07:27 +0530

    chore: install backend dependencies and node_modules

--- Pickify/backend/middlewares/isAuth.js
+++ Pickify/backend/middlewares/isAuth.js
@@ -1,21 +1,20 @@
 import jwt from 'jsonwebtoken'
 
-const isAuth = async (req,res,next) =>{
+const isAuth = async(req,res,next)=>{
     try {
-        const token = req.cookies.token
+        const token = req.cookies?.token;
         if(!token){
-            return res.status(401).json({message:"Token not Found"})
+            return res.status(401).json({message:"Token not found"})
         }
-        const decodeToken = jwt.verify(token,process.env.JWT_SECRET);
+        const decodeToken = jwt.verify(token,process.env.JWT_SECRET)
         if(!decodeToken){
-            return res.status(401).json({message:"Token not Found"})
+            return res.status(401).json({message:"User not found"})
         }
-        // console.log(decodeToken);
         req.userId = decodeToken.userId
-        next()
+        next();
     } catch (error) {
-        return res.status(500).json({message:"isAuth Error"})
+        return res.status(401).json({message:"Invalid or expired token"})
     }
-}
+} 
 
 export default isAuth
