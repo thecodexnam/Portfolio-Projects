a397e187da845c6446ec06f982fdcd1f580d89ea
namanydv <namanofficial91@gmail.com>
2026-05-17 11:58:33 +0530

    feat: implement full-stack architecture including authentication, shop/order management, and responsive dashboard components

--- Pickify/frontend/src/redux/store.js
+++ Pickify/frontend/src/redux/store.js
@@ -1,9 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './userSlice';
+import ownerReducer from './OwnerSlice';
+import cartReducer from './cartSlice';
 
 const store = configureStore({
     reducer: {
         auth: authReducer,
+        owner: ownerReducer,
+        cart: cartReducer,
     },
 });
 
